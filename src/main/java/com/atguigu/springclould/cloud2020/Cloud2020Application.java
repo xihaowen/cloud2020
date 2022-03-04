1ca5708b87992419949d9d36586a4fc4b8284c69
tom_glb <xihaowen_glb@outlook.com>
2022-03-04 18:43:33 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springclould/cloud2020/Cloud2020Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.springclould.cloud2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloud2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/springclould/cloud2020/Cloud2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.springclould.cloud2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
