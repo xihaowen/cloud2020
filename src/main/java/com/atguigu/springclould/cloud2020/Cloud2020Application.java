ea654fa868338cc70153118700c92d7184a2b348
xihaowen <100980342+xihaowen@users.noreply.github.com>
2022-03-05 10:09:56 +0800

    Update Cloud2020Application.java

--- src/main/java/com/atguigu/springclould/cloud2020/Cloud2020Application.java
+++ src/main/java/com/atguigu/springclould/cloud2020/Cloud2020Application.java
@@ -3,7 +3,7 @@ package com.atguigu.springclould.cloud2020;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Cloud2020Application {
 
     public static void main(String[] args) {
